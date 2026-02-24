b3654e20df7a3b4852aa62d3ec041493c97aaaf8
Karl Schriek <karl.schriek@snapcd.io>
2026-02-24 12:08:42 +0100

    set private_subnet_id to a different value

--- outputs.tf
+++ outputs.tf
@@ -10,7 +10,7 @@ output "public_subnet_id" {
 
 output "private_subnet_id" {
   description = "ID of the private subnet"
-  value       = random_uuid.private_subnet_id.result
+  value       = "a different value"
 }
 
 output "public_subnet_cidr" {
