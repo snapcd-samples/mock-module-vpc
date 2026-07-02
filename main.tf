35323e53e7d55089d70c5f0b95af88fd2db2d5bf
Karl Schriek <karl.schriek@snapcd.io>
2026-07-02 14:35:09 +0200

    Revert "Revert "remove some_other_id""
    This reverts commit 843ad0a199282a835f7e3dcca5df3a88d4e2c709.

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@ resource "random_uuid" "private_subnet_id" {
   depends_on = [time_sleep.wait_10s]
 }
 
-resource "random_uuid" "some_other_id" {
-  depends_on = [time_sleep.wait_10s]
-}
+# resource "random_uuid" "some_other_id" {
+#   depends_on = [time_sleep.wait_10s]
+# }
